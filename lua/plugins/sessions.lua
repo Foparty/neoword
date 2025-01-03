ae46000114f45e6df18daf257d089050fcae6602
fo <alfonsomartingarcia@gmail.com>
2025-01-03 14:32:26 +0000

    added auto-session plugin and statusline custom fn for filename

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    config = function()
+      require('auto-session').setup({
+        suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+        session_lens = {
+          load_on_startup = true,
+          theme_conf = { border = true },
+          previewer = false
+        },
+        vim.keymap.set('n', '<leader>ls', require('auto-session.session-lens').search_session)
+      })
+    end
+  }
+}
