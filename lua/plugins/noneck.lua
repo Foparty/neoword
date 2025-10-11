481dde11597a25ce1534e55ecfc9545ae1b17929
fo <103907142+Foparty@users.noreply.github.com>
2025-10-11 21:55:19 +0100

    no neck pain

---
+++ lua/plugins/noneck.lua
@@ -0,0 +1,14 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  keys = {
+    {
+      "<leader>cc",
+      "<cmd>NoNeckPain<cr>",
+      desc = "Centre buffer",
+    },
+  },
+  opts = {
+    width = 100,
+    noremap = true,
+  },
+}
